fc90bc3c3317698c35204e13ca9d0a7b9bd7735b
suxianhua <1076395712@qq.com>
2020-10-23 15:57:57 +0800

    add RelayNetwork

---
+++ Core/Core/Block/RelayNetwork.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.Net;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ETModel
+{
+    public class RelayNetwork : Component
+    {
+        ComponentNetworkInner relayNetworkInner = null;
+
+        public override void Awake(JToken jd = null)
+        {
+            ComponentNetMsg componentNetMsg = Entity.Root.GetComponent<ComponentNetMsg>(); // 一级网络
+            componentNetMsg.registerMsg(NetOpcode.P2P_NewBlock, P2P_NewBlock_Handle);
+
+        }
+
+        public override void Start()
+        {
+            ComponentNetMsg componentNetMsg = this.entity.GetComponent<ComponentNetMsg>();// 中继网络
+            componentNetMsg.registerMsg(NetOpcode.Q2P_New_Node, Q2P_New_Node_Handle);
+
+            relayNetworkInner = this.entity.GetComponent<ComponentNetworkInner>();
+            Log.Info($"RelayNetwork.Start {relayNetworkInner.ipEndPoint}");
+        }
+
+        void P2P_NewBlock_Handle(Session session, int opcode, object msg)
+        {
+            P2P_NewBlock p2p_Block = msg as P2P_NewBlock;
+
+            //var newBlock = new P2P_NewBlock() { block = p2p_Block.block, ipEndPoint = p2p_Block.ipEndPoint };
+            var newBlock = new P2P_NewBlock() { block = p2p_Block.block, ipEndPoint = Entity.Root.GetComponent<ComponentNetworkInner>().ipEndPoint.ToString() };
+
+            relayNetworkInner.BroadcastToAll(newBlock);
+        }
+
+        void Q2P_New_Node_Handle(Session session, int opcode, object msg)
+        {
+            Q2P_New_Node new_Node = msg as Q2P_New_Node;
+            //Log.Debug($"Q2P_New_Nod {new_Node.ActorId} \r\nHash: {new_Node.HashCode}");
+
+            R2P_New_Node response = new R2P_New_Node() { Nodes = "", sendTime = new_Node.sendTime, nodeTime = TimeHelper.Now() };
+            session.Reply(new_Node, response);
+        }
+
+
+
+    }
+
+
+}
